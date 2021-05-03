ffaadd6a00c0a234475f398f71f92f405a901c75
cambrown101 <cameron-brown@live.com>
2021-05-03 16:47:10 -0700

    remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
 import WhyAcclamation from "./components/WhyAcclamation/WhyAcclamation";
-import HowWeWork from "./components/LandingPage/LandingComponents/HowWeWork/HowWeWork";
-import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
 
 function App() {
   return (
