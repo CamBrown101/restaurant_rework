776ffd152aca111aab39cc8188c5dd0bed4f5ea6
cambrown101 <cameron-brown@live.com>
2021-05-03 19:01:18 -0700

    Remove unused components

--- src/components/Carousel/Carousel.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Carousel from './Carousel';
-
-export default {
-  title: 'Carousel',
-  component: Carousel,
-};
-
-export const Default = () => <Carousel interval={3000} numberOfSlides={5} />;
--- src/components/LandingPage/LandingComponents/FromConcept/FromConcept.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import FromBrand from "./FromConcept";
-
-export default {
-  title: "FromBrand",
-  component: FromBrand,
-};
-
-export const Default = () => <FromBrand />;
