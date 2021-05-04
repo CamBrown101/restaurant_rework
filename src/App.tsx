baa60a51ae65d9a573d82c24e696eb4ae812ca54
cambrown101 <cameron-brown@live.com>
2021-05-03 19:42:28 -0700

    Remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
-import WhyAcclamation from "./components/WhyAcclamation/WhyAcclamation";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={LandingPage} />
-        <Route path="/View-Menu" exact component={WhyAcclamation} />
+        <Route path="/View-Menu" exact component={LandingPage} />
       </Switch>
       <NavBar />
     </Router>
