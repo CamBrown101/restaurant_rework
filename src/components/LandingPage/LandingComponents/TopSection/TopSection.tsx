08031528d2e5e54300a07c3a5e45f2e1c165e503
cambrown101 <cameron-brown@live.com>
2021-05-03 19:01:56 -0700

    Rough out top section

--- src/components/LandingPage/LandingComponents/TopSection/TopSection.tsx
+++ src/components/LandingPage/LandingComponents/TopSection/TopSection.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import bayView from "../../../../images/bayView.jpg";
+import fullColorLogo from "../../../../images/Riva-Icon-FullColour-trimmed.png";
 
 const HomePage = () => {
   return (
     <AboutContainer>
       <AboutContainerInner>
-        <h2 className="about-header">
-          THE CONNECTION BETWEEN YOUR F&B BUSINESS AND YOUR IDEAL CUSTOMER
-        </h2>
-        <p className="about-content">
-          From branding and creative to marketing strategy, Acclamation can
-          design and initiate your brands vision with your goals in mind.
-          Diverse solutions to every hurdle your business encounters, from
-          online footprint to social media engagement.
-        </p>
-        <Button>WHY ACCLAMATION</Button>
+        <img className="logo" src={fullColorLogo} alt="Riva logo" />
       </AboutContainerInner>
     </AboutContainer>
   );
@@ -22,56 +15,28 @@ const HomePage = () => {
 export default HomePage;
 
 const AboutContainer = styled.header`
-  background-color: black;
-  max-width: 100%;
   width: 100%;
-  color: #fff;
-  height: 675px;
+  height: 700px;
   padding-top: 120px;
   display: flex;
   justify-content: center;
   align-items: center;
-  .about-header {
-    width: 50%;
-    margin-bottom: 20px;
-    font-weight: 900;
-    text-transform: uppercase;
-    font-size: 42px;
-    line-height: 44px;
-  }
-  .about-content {
-    width: 50%;
-    font-size: 24px;
-    line-height: 32px;
-    font-weight: 500;
-  }
 `;
 const AboutContainerInner = styled.div`
+  background-image: url(${bayView});
+  background-size: 100% 100%;
   background-repeat: no-repeat;
-  background-size: 600px;
-  background-position-x: right;
-  background-position-y: center;
-  width: 90%;
+  width: 100%;
   height: 100%;
-  max-width: 1400px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-`;
-const Button = styled.button`
-  margin-bottom: 20px;
-  margin-top: 30px;
-  font-size: 24px;
-  line-height: 32px;
-  color: #fff;
-  background-color: #b09a7f;
-  padding-top: 12px;
-  padding: 9px 15px;
-  font-weight: 700;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  cursor: pointer;
-  border: 0;
-  font-family: "Work Sans", sans-serif;
-  width: 283px;
+  align-items: center;
+
+  .logo {
+    background-color: rgba(255, 255, 255, 0.3);
+    height: 60%;
+    width: 30%;
+    padding: 20px;
+  }
 `;
