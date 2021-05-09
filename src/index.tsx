f5b6486c707bb3de8fc6e7d68a234fb30635a7d7
cambrown101 <cameron-brown@live.com>
2021-05-09 15:33:01 -0700

    import new fonts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "typeface-raleway";
+import "typeface-josefin-sans";
+import "typeface-forum";
 
 ReactDOM.render(
   <React.StrictMode>
