5fbfdeb57200cc8a630b90427e93717be631fa49
cambrown101 <cameron-brown@live.com>
2021-05-09 16:48:17 -0700

    Create large Dish Data

---
+++ src/components/Menu/LargeDishes.tsx
@@ -0,0 +1,50 @@
+const largeDishData = {
+  title: "Grandi",
+  english: "Large Plates",
+  items: [
+    {
+      name: "Saltimbocca",
+      english: "Prosciutto Wrapped Salmon",
+      price: "22",
+      description:
+        "sockeye salmon fillet, sage leaves, prosciutto, baguette, lemon butter-caper-white wine sauce**",
+    },
+    {
+      name: "Misto",
+      english: "Mixed Seafood",
+      price: "27",
+      description:
+        "braised whole fresh water trout, mussels, clams, baby scallops, heirloom tomatoes, garlic, fingerling potatoes, kale, basil**",
+    },
+    {
+      name: "Braciola",
+      english: "Pork Chop",
+      price: "25",
+      description:
+        "crispy bone-in pork cutlet, heirloom tomatoes, basil, bocconcini, evoo, lemon, basil",
+    },
+    {
+      name: "Bistecca",
+      english: "Sliced Steak",
+      price: "27",
+      description:
+        "shaved ribeye, arugula, fresh lemon juice, evoo, heirloom tomatoes, grana padano**",
+    },
+    {
+      name: "Risotto",
+      english: "Braised Barley",
+      price: "19",
+      description:
+        "pearl barley, chicken breast, wild mushrooms, shallots, garlic, white wine, thyme, fresh parsley, grana padano**",
+    },
+    {
+      name: "Lasagna",
+      english: "Baked Pasta",
+      price: "22",
+      description:
+        "made to order, bison, ricotta, roma tomato, basil, tomato sauce, mozzarella crust",
+    },
+  ],
+};
+
+export default largeDishData;
