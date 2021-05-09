34bbb166e45feed8c391fd24324a5af8700fb2ca
cambrown101 <cameron-brown@live.com>
2021-05-09 16:17:47 -0700

    Add vegetable dishes info

---
+++ src/components/Menu/VegetableDishes.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const VegetableDishes = () => {
+  return (
+    <div>
+      <h2>Verdure</h2> <h6>Vegetable dishes</h6>
+      <h3>Patata</h3> <h6>Fingerling potato</h6> <h5>10</h5>
+      <p>
+        crispy broken potatoes, garlic, parmesan, roast red pepper aioli,
+        herbs**
+      </p>
+      <h3>Cavolfiore</h3> <h6>Cauliflower</h6> <h5>10</h5>
+      <p>roasted cauliflower florets, tahini crème, dates, fresh herbs**</p>
+      <h3>Asparago</h3> <h6>Asparagus</h6> <h5>12</h5>
+      <p>
+        asparagus spears, garlic, grana padano, grape tomato, pinenuts, lemon
+        juice, butter**
+      </p>
+      <h3>Olives</h3> <h6>Assorted Olives</h6> <h5>11</h5>
+      <p>rosemary & lemon marinade, garlic, warm evoo, assorted olives**</p>
+      <h3>Polenta</h3> <h6>Polenta Fries</h6> <h5>12</h5>
+      <p>
+        soft and fluffy polenta, fried till crispy, roast red pepper aioli**
+      </p>
+      <h3>Burrata</h3> <h6>Mozzarella & Cream</h6> <h6>19</h6>
+      <p>
+        fresh cheese, maple roasted squash, toasted almonds, Evoo, crispy sage**
+      </p>
+    </div>
+  );
+};
