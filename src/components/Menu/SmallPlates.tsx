4d3d25a24c704bad341684f5c48086303a37e8d9
cambrown101 <cameron-brown@live.com>
2021-05-09 16:17:36 -0700

    Add small plate menu information

---
+++ src/components/Menu/SmallPlates.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const SmallPlates = () => {
+  return (
+    <div>
+      <h2>Piccoli</h2> <h6>Small Plates</h6>
+      <h3>Polpette</h3> <h6>Meatballs</h6> <h5>15</h5>
+      <p>
+        veal pork & beef meatballs, baby bocconcini, tomato sauce, ricotta
+        crema, fresh basil
+      </p>
+      <h2>Calamari Fried</h2>
+      <h6>squid</h6>
+      <h5>19</h5>
+      <p>crispy graham cracker crust, Italian parsley, tomato dipping sauce</p>
+      <h2>Arancini</h2> <h6>Risotto Croquettes</h6> <h5>14</h5>
+      <p>crispy fried barley risotto, bison, ricotta, tomato sauce</p>
+      <h2>Cozze</h2> <h6>Mussels</h6>
+      <h5>21</h5>
+      <p>
+        salt spring mussels, dry white wine, shallots, roma tomato, garlic,
+        parsley**
+      </p>
+      <h2>Carpaccio</h2> <h6>Raw Filet Mignon</h6> <h5>22</h5>
+      <p>
+        beef tenderloin, arugula, balsamic dijon vinaigrette, grana padano,
+        crispy capers**
+      </p>
+      <h2>Formaggio</h2> <h6>Pane Cheese Bread</h6> <h5>11</h5>
+      <p>
+        house made Focaccia bread, mozzarella, grana padano, fresh basil,
+        marinara dip
+      </p>
+      <h2>Gamberi</h2> <h6>Prawns</h6> <h5>17</h5>
+      <p>
+        jumbo prawns, licorice liqueur, shallots, roma tomato, white wine, lemon
+        juice, fennel leaves, basil, butter**
+      </p>
+      <h2>Bruschetta</h2> <h6>Tomato Canape</h6> <h5>15</h5>
+      <p>
+        roma tomatoes, slivered garlic, fresh basil, maple balsamic reduction,
+        crostinis
+      </p>
+      <h2>Bruxelles Brussel</h2> <h6>Sprouts</h6> <h5>14</h5>
+      <p>
+        pancetta, pistachios, shallots, butter, sweet and tangy red wine vinegar
+        glaze**
+      </p>
+    </div>
+  );
+};
