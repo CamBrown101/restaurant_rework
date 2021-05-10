7fbf4d94a1b2e50a47b6a44127994a584fe9f2db
cambrown101 <cameron-brown@live.com>
2021-05-10 08:00:58 -0700

    Add desert data

---
+++ src/components/Menu/dessert.tsx
@@ -0,0 +1,34 @@
+const dessertData = {
+  title: "Dolce",
+  english: "Dessert",
+  items: [
+    {
+      name: "Tiramisu",
+      english: "Coffee Cake & Custard",
+      price: "9",
+      description:
+        "marsala wine sabayon, lady fingers, kahlua, espresso, cocoa ",
+    },
+    {
+      name: "Mousse Duo",
+      english: "Chocolate Mousse",
+      price: "9",
+      description:
+        "dark chocolate mousse, amaretto, white chocolate mousse, pizzelle waffle** ",
+    },
+    {
+      name: "Panna Cotta",
+      english: "Cream Custard",
+      price: "9",
+      description: "greek yogurt, heavy cream, fresh vanilla bean ** ",
+    },
+    {
+      name: "Affogato",
+      english: "Espresso & Gelato",
+      price: "9",
+      description: "vanilla Bean Gelato, espresso**",
+    },
+  ],
+};
+
+export default dessertData;
