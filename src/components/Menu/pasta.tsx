cb77628b3fe500c55726ba82a6707a10d16987a3
cambrown101 <cameron-brown@live.com>
2021-05-10 08:01:05 -0700

    Add pasta data

---
+++ src/components/Menu/pasta.tsx
@@ -0,0 +1,70 @@
+const pastaData = {
+  title: "Pasta",
+  english: "",
+  items: [
+    {
+      name: "Puttanesca",
+      english: "Lady of the Night",
+      price: "18",
+      description:
+        "spaghettini, olives, capers, garlic, anchovy, heirloom tomatoes, tomato sauce, basil**",
+    },
+    {
+      name: "Frutti di mare",
+      english: "Seafood",
+      price: "23",
+      description:
+        "fettucini, clams, mussels, squid, garlic, scallops, tomato sauce, fresh basil**",
+    },
+    {
+      name: "Pomodoro",
+      english: "Fresh Tomato",
+      price: "17",
+      description:
+        "tagliatelle, heirloom tomatoes, garlic, evoo, fresh basil**",
+    },
+    {
+      name: "Vongole",
+      english: "Clams",
+      price: "20",
+      description:
+        "bucatini, clams, garlic, white wine, fresh parsley, evoo, grana padano**",
+    },
+    {
+      name: "Carbonara",
+      english: "Bacon & Egg",
+      price: "19",
+      description:
+        "rigatoni, pancetta, egg yolk, grana padano, fresh cracked pepper**",
+    },
+    {
+      name: "Aglio e olio",
+      english: "Garlic & Oil",
+      price: "16",
+      description:
+        "spaghettini, mild peppers, slivered garlic, anchovy, evoo, grana padano**",
+    },
+    {
+      name: "Salsiccia",
+      english: "Sausage",
+      price: "18",
+      description:
+        "bucatini, Sweet italian sausage, white wine, shallots, heavy cream, kale, grana padano**",
+    },
+    {
+      name: "Ravioli",
+      english: "Stuffed Pasta",
+      price: "22",
+      description: "cotechino, sage, brown butter",
+    },
+    {
+      name: "Gnocchi",
+      english: "Dumpling Pasta",
+      price: "20",
+      description:
+        "potato dumplings, bison bolognese, red wine, basil, grana padano",
+    },
+  ],
+};
+
+export default pastaData;
