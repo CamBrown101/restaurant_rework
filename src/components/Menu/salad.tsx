c6915faacd10e7a1931e04351972ca98c7c3305d
cambrown101 <cameron-brown@live.com>
2021-05-10 08:01:17 -0700

    Add salad data

---
+++ src/components/Menu/salad.tsx
@@ -0,0 +1,29 @@
+const saladData = {
+  title: "Insalata",
+  english: "Salad",
+  items: [
+    {
+      name: "Caprese",
+      english: "Tomato & Cheese Salad",
+      price: "16",
+      description:
+        "heirloom tomatoes, buffalo mozzarella, basil leaves, evoo, maple balsamic reduction**",
+    },
+    {
+      name: "Panzanella",
+      english: "Tuscan Bread Salad",
+      price: "16",
+      description:
+        "shaved fennel, arugula, heirloom tomatoes, toasted bread, basil, parmigiana, red wine vinaigrette **",
+    },
+    {
+      name: "Caesar",
+      english: "Anchovy & Garlic Salad",
+      price: "9/16",
+      description:
+        "kale, anchovy vinaigrette, toasted panko, prosciutto crisps, red wine egg**",
+    },
+  ],
+};
+
+export default saladData;
