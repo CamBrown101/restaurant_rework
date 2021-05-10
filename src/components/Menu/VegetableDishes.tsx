8b070e91b36572c874c21b0350f12cfc729f8217
cambrown101 <cameron-brown@live.com>
2021-05-09 17:06:12 -0700

    Make vegetable dishes into object data

--- src/components/Menu/VegetableDishes.tsx
+++ src/components/Menu/VegetableDishes.tsx
@@ -1,31 +1,50 @@
-import React from "react";
-
-export const VegetableDishes = () => {
-  return (
-    <div>
-      <h2>Verdure</h2> <h6>Vegetable dishes</h6>
-      <h3>Patata</h3> <h6>Fingerling potato</h6> <h5>10</h5>
-      <p>
-        crispy broken potatoes, garlic, parmesan, roast red pepper aioli,
-        herbs**
-      </p>
-      <h3>Cavolfiore</h3> <h6>Cauliflower</h6> <h5>10</h5>
-      <p>roasted cauliflower florets, tahini crème, dates, fresh herbs**</p>
-      <h3>Asparago</h3> <h6>Asparagus</h6> <h5>12</h5>
-      <p>
-        asparagus spears, garlic, grana padano, grape tomato, pinenuts, lemon
-        juice, butter**
-      </p>
-      <h3>Olives</h3> <h6>Assorted Olives</h6> <h5>11</h5>
-      <p>rosemary & lemon marinade, garlic, warm evoo, assorted olives**</p>
-      <h3>Polenta</h3> <h6>Polenta Fries</h6> <h5>12</h5>
-      <p>
-        soft and fluffy polenta, fried till crispy, roast red pepper aioli**
-      </p>
-      <h3>Burrata</h3> <h6>Mozzarella & Cream</h6> <h6>19</h6>
-      <p>
-        fresh cheese, maple roasted squash, toasted almonds, Evoo, crispy sage**
-      </p>
-    </div>
-  );
+const largeDishData = {
+  title: "Verdure",
+  english: "Vegetable dishes",
+  items: [
+    {
+      name: "Patata",
+      english: "Fingerling potato",
+      price: "10",
+      description:
+        "crispy broken potatoes, garlic, parmesan, roast red pepper aioli, herbs**",
+    },
+    {
+      name: "Cavolfiore",
+      english: "Cauliflower",
+      price: "10",
+      description:
+        "roasted cauliflower florets, tahini crème, dates, fresh herbs**",
+    },
+    {
+      name: "Asparago",
+      english: "Asparagus",
+      price: "12",
+      description:
+        "asparagus spears, garlic, grana padano, grape tomato, pinenuts, lemon juice, butter**",
+    },
+    {
+      name: "Olives",
+      english: "Assorted Olives",
+      price: "11",
+      description:
+        "rosemary & lemon marinade, garlic, warm evoo, assorted olives**",
+    },
+    {
+      name: "Polenta",
+      english: "Polenta Fries",
+      price: "12",
+      description:
+        "soft and fluffy polenta, fried till crispy, roast red pepper aioli**",
+    },
+    {
+      name: "Burrata",
+      english: "Mozzarella & Cream",
+      price: "19",
+      description:
+        "fresh cheese, maple roasted squash, toasted almonds, Evoo, crispy sage**",
+    },
+  ],
 };
+
+export default largeDishData;
