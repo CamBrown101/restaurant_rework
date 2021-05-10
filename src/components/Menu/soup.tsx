c03184155c2c0c2b65d25ee2ac4a86e9242a1b15
cambrown101 <cameron-brown@live.com>
2021-05-10 08:00:51 -0700

    Add soup data

---
+++ src/components/Menu/soup.tsx
@@ -0,0 +1,22 @@
+const soupData = {
+  title: "Zuppa",
+  english: "Soup",
+  items: [
+    {
+      name: "Minestrone",
+      english: "Broth soup",
+      price: "7/12",
+      description:
+        "pancetta, chicken broth, parmesan rind, diced vegetables, kidney beans, garbanzo beans**",
+    },
+    {
+      name: "Pesce",
+      english: "Cream Soup",
+      price: "9/15",
+      description:
+        "baby clams, sweet italian sausage, flaky fish, potatoes, cream broth",
+    },
+  ],
+};
+
+export default soupData;
