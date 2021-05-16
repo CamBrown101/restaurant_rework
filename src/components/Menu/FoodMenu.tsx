04649d8066c05d1b4e7de088a7519462cb257d79
cambrown101 <cameron-brown@live.com>
2021-05-16 12:40:08 -0700

    Add carousel

--- src/components/Menu/FoodMenu.tsx
+++ src/components/Menu/FoodMenu.tsx
@@ -1,12 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import Carousel from "../Carousel/Carousel";
+import FoodMenu1 from "../../images/menus/Riva-Menu-Food1.png";
+import FoodMenu2 from "../../images/menus/Riva-Menu-Food2.png";
+import FoodMenu3 from "../../images/menus/Riva-Menu-Dessert.png";
+
+const slideData = [
+  {
+    imageUrl: FoodMenu1,
+    altText: "Riva food menu",
+  },
+  {
+    imageUrl: FoodMenu2,
+    altText: "Riva food menu",
+  },
+  {
+    imageUrl: FoodMenu3,
+    altText: "Riva food menu",
+  },
+];
 
 const FoodMenu = () => {
   return (
-    <div>
-      <p>Food Menu</p>
-    </div>
+    <FoodMenuWrapper>
+      <FoodMenuContainerInner>
+        <Carousel interval={10000} numberOfSlides={3} data={slideData} />
+      </FoodMenuContainerInner>
+    </FoodMenuWrapper>
   );
 };
 
 export default FoodMenu;
+
+const FoodMenuWrapper = styled.main`
+  width: 100%;
+  height: 700px;
+  padding-top: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+const FoodMenuContainerInner = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
