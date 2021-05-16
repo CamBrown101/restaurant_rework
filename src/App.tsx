bb11dcf4ca63c74dba2ad134bfdbb765c988d038
cambrown101 <cameron-brown@live.com>
2021-05-16 11:30:12 -0700

    Add routes for nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
+import FoodMenu from "./components/Menu/FoodMenu";
+import DrinkMenu from "./components/Menu/DrinkMenu";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={LandingPage} />
-        <Route path="/View-Menu" exact component={LandingPage} />
+        <Route path="/View-Food-Menu" exact component={FoodMenu} />
+        <Route path="/View-Drink-Menu" exact component={DrinkMenu} />
+        <Route path="/Reservations" exact component={LandingPage} />
+        <Route path="/Order-Online" exact component={LandingPage} />
       </Switch>
       <NavBar />
     </Router>
