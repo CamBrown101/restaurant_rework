54e4fbf198cb3d5b2d4b562d709e424722505454
cambrown101 <cameron-brown@live.com>
2021-05-16 12:58:50 -0700

    Add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+  withRouter,
+} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
 import FoodMenu from "./components/Menu/FoodMenu";
 import DrinkMenu from "./components/Menu/DrinkMenu";
 
+function _ScrollToTop(props: any) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children;
+}
+const ScrollToTop = withRouter(_ScrollToTop);
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/View-Food-Menu" exact component={FoodMenu} />
-        <Route path="/View-Drink-Menu" exact component={DrinkMenu} />
-        <Route path="/Reservations" exact component={LandingPage} />
-        <Route path="/Order-Online" exact component={LandingPage} />
-      </Switch>
+      <ScrollToTop>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/View-Food-Menu" exact component={FoodMenu} />
+          <Route path="/View-Drink-Menu" exact component={DrinkMenu} />
+          <Route path="/Reservations" exact component={LandingPage} />
+          <Route path="/Order-Online" exact component={LandingPage} />
+        </Switch>
+      </ScrollToTop>
       <NavBar />
     </Router>
   );
