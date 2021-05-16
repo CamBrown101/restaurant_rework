239de5a8310958cfe4ad2a1b44b3a73553902f86
cambrown101 <cameron-brown@live.com>
2021-05-16 11:33:05 -0700

    Create FoodMenu component

---
+++ src/components/Menu/FoodMenu.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const FoodMenu = () => {
+  return (
+    <div>
+      <p>Food Menu</p>
+    </div>
+  );
+};
+
+export default FoodMenu;
