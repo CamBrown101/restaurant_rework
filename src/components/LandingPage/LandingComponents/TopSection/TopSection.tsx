58425c5c1b16b289f8e8780d676532d36bfcddc1
cambrown101 <cameron-brown@live.com>
2021-05-21 10:33:58 -0700

    Change hero image

--- src/components/LandingPage/LandingComponents/TopSection/TopSection.tsx
+++ src/components/LandingPage/LandingComponents/TopSection/TopSection.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import bayView2 from "../../../../images/heroImage001.jpg";
+import bayView2 from "../../../../images/heroImage002.png";
 
 const HomePage = () => {
   return (
     <AboutContainer>
-      <AboutContainerInner></AboutContainerInner>
+      <AboutContainerInner>
+        <img src={bayView2} alt="Redifing Waterfront Dining" />
+      </AboutContainerInner>
     </AboutContainer>
   );
 };
@@ -13,28 +15,25 @@ export default HomePage;
 
 const AboutContainer = styled.header`
   width: 100%;
-  height: 700px;
+  height: 100%;
   padding-top: 120px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 const AboutContainerInner = styled.div`
-  background-image: url(${bayView2});
+  /* background-image: url(${bayView2});
   background-size: cover;
   background-position: center;
-  background-repeat: no-repeat;
-  width: 100%;
-  height: 100%;
+  background-repeat: no-repeat; */
+  width: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  .logo {
-    background-color: rgba(255, 255, 255, 0.65);
-    height: 60%;
-    width: auto;
-    padding: 20px;
+  img {
+    width: 100%;
+    height: auto;
   }
 `;
